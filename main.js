8f063565babd73fe9d581b910391e0aa408e9488
KauaHenriqueC <kaua2hcantanhede@gmail.com>
2026-04-28 06:53:21 -0300

    [FEAT]: live hours[FIX]: size of hours

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-var now = new Date()    // get computer date
+function updateClock() {    // Function to update the clock
+  var now = new Date()      // Get computers time
+  var hours = document.querySelector('.hours')  // Get element in html
+
+  hours.innerText = now.toLocaleTimeString().replace(/[AM, PM]/g, '') // Remove AM and PM
+}
+
+setInterval(updateClock, 1000)  // Run the function for 1 second
+updateClock() // Start
